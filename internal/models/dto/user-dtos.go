f87e8f1a5c5f7181f7910f0afa30f85a27924eed
Hemang Bhagat <bhagathemang360@gmail.com>
2025-11-17 23:10:56 +0530

    fix: Update Current Year validation range (#32)

--- internal/models/dto/user-dtos.go
+++ internal/models/dto/user-dtos.go
@@ -4,7 +4,7 @@ type CreateUserRequest struct {
 	Name         string `json:"name" validate:"required"`
 	USN          string `json:"usn" validate:"required"`
 	MobileNumber string `json:"mobile_number" validate:"required"`
-	CurrentYear  int    `json:"current_year" validate:"required,min=2000,max=2100"`
+	CurrentYear  int    `json:"current_year" validate:"required,min=1,max=3"`
 	Department   string `json:"department" validate:"required"`
 }
 
